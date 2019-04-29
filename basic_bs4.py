a8fe04926b9f747c65df890dbca0e68b6e237d78
JhonnathaMonteiro <jhonnatha.am@gmail.com>
2019-04-28 21:02:39 -0300

    Comm

--- basic_bs4.py
+++ basic_bs4.py
@@ -2,36 +2,38 @@ import csv
 import requests
 from bs4 import BeautifulSoup
 
-# Criar um arquivo para gravar os dados, adicionar linha de cabecalhos
+# Create a file to store the data and put the header
 f = csv.writer(open('z-artist-names.csv', 'w'))
 f.writerow(['Name', 'Links'])
 
 pages = []
 
+# Create a list with the links of all pages to be scrapped
 for i in range(1, 5):
     url = 'https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ' + \
         str(i) + '.htm'
     pages.append(url)
 
+# Loop through all items in "pages" variable
 for item in pages:
-    # Coletar e analisar a primeira página
+    # Colect and analysis the item (page)
     page = requests.get(item)
     soup = BeautifulSoup(page.text, 'lxml')
 
-    # Remover links inferiores indesejados
+    # Remove unnecessary items
     last_links = soup.find(class_='AlphaNav')
     last_links.decompose()
 
-    # Pegar todo o texto da div BodyText
+    # Take all elementos from div BodyText
     artist_name_list = soup.find(class_='BodyText')
 
-    # Pegar o texto de todas as instâncias da tag <a> dentro da div BodyText
+    # Retrive the text of all <a> tag instances inside the div BodyText
     artist_name_list_items = artist_name_list.find_all('a')
 
-    # Usar .contents para pegar as tags <a> filhas
+    # Using .contents to retrive <a> child tag (name and link in this example)
     for artist_name in artist_name_list_items:
         name = artist_name.contents[0]
         links = 'https://web.archive.org' + artist_name.get('href')
 
-        # Adicionar em uma linha o nome de cada artista e o link associado
+        # Write in the CSV file
         f.writerow([name, links])
